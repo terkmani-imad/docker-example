ed93c5c2e9ab19f88932582c67c704242ea2fdc2
terkmani-imad <76059477+terkmani-imad@users.noreply.github.com>
2025-11-17 11:19:50 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  const currentDateTime = new Date().toLocaleString();
+  res.send(`Hello World, it is ${currentDateTime}`);
+});
+
+// Only start the server if this file is run directly
+if (import.meta.url === `file://${process.argv[1]}`) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
